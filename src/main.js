f1718cefea7b4baf74ff2dc41ebaf5a34ae49e50
Ecy-zp <2763936916@qq.com>
2021-03-16 21:00:49 +0800

    引入elementUI

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入element
+import ElementUI from 'element-ui'
+// // 引入Element 主题文件
+// import 'element-ui/lib/theme-chalk/index.css'
+
+// 引入自身样式文件
+import './styles/index.scss'
+// 将Element注册为Vue组件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
