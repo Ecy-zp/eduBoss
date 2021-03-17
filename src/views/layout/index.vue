d3b8a886d3fd7754ac9829ef5d5679796d59db46
Ecy-zp <2763936916@qq.com>
2021-03-17 13:48:31 +0800

    布局容器页面修改

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,13 +1,47 @@
 <template>
-  <div>
-    <!-- 子路由出口 -->
-    <router-view/>
-  </div>
+<el-container>
+  <el-aside width="200px">
+    <!-- 侧边栏 -->
+    <app-aside></app-aside>
+  </el-aside>
+  <el-container>
+    <el-header>
+      <app-header></app-header>
+    </el-header>
+    <el-main>
+      <!-- 设置子路由出口 -->
+      <router-view></router-view>
+    </el-main>
+  </el-container>
+</el-container>
 </template>
+
 <script>
+// 引入侧边栏组件
+import AppAside from './components/AppAside'
+// 引入头部组件
+import AppHeader from './components/AppHeader'
+
 export default {
+  name: 'Layout',
+  components: {
+    AppAside,
+    AppHeader
+  }
 }
 </script>
-<style lang="sass" scoped>
 
+<style lang="scss" scoped>
+  .el-container {
+    height:100vh;
+  }
+  .el-aside {
+    background-color: #eee666;
+  }
+  .el-header {
+    background-color: #fff;
+  }
+  .el-main {
+    background-color: #ddd;
+  }
 </style>
