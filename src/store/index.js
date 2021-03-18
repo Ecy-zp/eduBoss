657b9f2a318efc4a9bbe49422b557be6cb226c16
Ecy-zp <2763936916@qq.com>
2021-03-18 13:01:11 +0800

    VueX初始化

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 用于在登录成功后保存用户信息（初始只尝试读取本地存储）
+    user: JSON.parse(window.localStorage.getItem('user') || null)
   },
   mutations: {
+    // 设置用户数据
+    setUser (state, payload) {
+      state.user = JSON.parse(payload)
+      // 将payload添加到到本地存储中
+      window.localStorage.setItem('user', payload)
+    }
   },
   actions: {
   },
