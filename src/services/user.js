3c5c8d45ba00b8c99a3b2da1ceea9e19b80fdda5
Ecy-zp <2763936916@qq.com>
2021-03-18 13:00:08 +0800

    封装请求方法

---
+++ src/services/user.js
@@ -0,0 +1,12 @@
+// 封装请求方法
+import request from '@/utils/request'
+import qs from 'qs'
+
+// 用户登录接口
+export const login = data => {
+  return request({
+    method: 'POST',
+    url: '/front/user/login',
+    data: qs.stringify(data)
+  })
+}
