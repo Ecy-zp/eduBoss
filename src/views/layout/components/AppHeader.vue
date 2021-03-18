28e94821d548ac9e2690ba4744f66d61a7a96c0f
Ecy-zp <2763936916@qq.com>
2021-03-18 19:55:23 +0800

    用户信息和登出功能增加

--- src/views/layout/components/AppHeader.vue
+++ src/views/layout/components/AppHeader.vue
@@ -11,20 +11,60 @@
     <!-- 右侧下拉菜单 -->
     <span class="el-dropdown-link">
         <!-- 用户头像 -->
-        <el-avatar :size="30" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"></el-avatar>
+        <el-avatar :size="30" :src= "userInfo.portrait || 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png' "></el-avatar>
         <i class="el-icon-arrow-down el-icon--right"></i>
     </span>
     <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item>用户信息</el-dropdown-item>
-        <el-dropdown-item divided>登出</el-dropdown-item>
+        <el-dropdown-item>{{ userInfo.userName }}</el-dropdown-item>
+        <el-dropdown-item divided @click.native="Logout">登出</el-dropdown-item>
     </el-dropdown-menu>
     </el-dropdown>
   </div>
 </template>
 
 <script>
+import { getUserInfo } from '@/services/user'
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+  created () {
+    // 加载用户信息
+    this.loadUserInfo()
+    // 下面是为了功能测试
+    this.loadUserInfo()
+  },
+  data () {
+    return {
+      userInfo: {}
+    }
+  },
+  methods: {
+    async loadUserInfo () {
+      const { data } = await getUserInfo()
+      this.userInfo = data.content
+    },
+    // 退出
+    Logout () {
+      this.$confirm('确认退出?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 1.清除store中的数据
+        this.$store.commit('setUser', null)
+        // 2.跳转登录页面
+        this.$router.push('/login')
+        this.$message({
+          type: 'success',
+          message: '退出成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消退出'
+        })
+      })
+    }
+  }
 }
 </script>
 
