82d14800dc05c52e9b8c028451cbe72dcf970a94
Ecy-zp <2763936916@qq.com>
2021-03-18 19:52:11 +0800

    添加获取用户信息的接口

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,7 @@
 // 封装请求方法
 import request from '@/utils/request'
 import qs from 'qs'
+// import store from '@/store'
 
 // 用户登录接口
 export const login = data => {
@@ -8,5 +9,13 @@ export const login = data => {
     method: 'POST',
     url: '/front/user/login',
     data: qs.stringify(data)
+  })
+}
+
+// 用户基本信息接口
+export const getUserInfo = () => {
+  return request({
+    method: 'GET',
+    url: '/front/user/getInfo'
   })
 }
