aa4f81dc4565eb3495359cd82ff1cfd11fd72d7e
Ecy-zp <2763936916@qq.com>
2021-03-20 20:58:18 +0800

    菜单功能接口

---
+++ src/services/menu.js
@@ -0,0 +1,38 @@
+// 菜单接口
+import request from '@/utils/request'
+
+// 获取菜单页面信息
+export const getEditMenuInfo = (id = -1) => {
+  return request({
+    method: 'GET',
+    url: '/boss/menu/getEditMenuInfo', // 另一种写法url: '/boss/menu/getEditMenuInfo?id=${id}'
+    params: {
+      id
+    }
+  })
+}
+
+// 添加菜单接口
+export const createOrUpdateMenu = data => {
+  return request({
+    method: 'POST',
+    url: '/boss/menu/saveOrUpdate',
+    data
+  })
+}
+
+// 添加获取所有菜单接口
+export const getAllMenus = () => {
+  return request({
+    method: 'GET',
+    url: '/boss/menu/getAll'
+  })
+}
+
+// 删除菜单接口
+export const deleteMenu = id => {
+  return request({
+    method: 'DELETE',
+    url: `/boss/menu/${id}`
+  })
+}
