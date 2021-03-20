0e4033026d2ac424e9d123595d8a78437faf6378
Ecy-zp <2763936916@qq.com>
2021-03-20 20:56:56 +0800

    编辑菜单页面

---
+++ src/views/menu/edit.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="menu-edit">
+    <!--  -->
+    <create-or-edit :isEdit="true"></create-or-edit>
+  </div>
+</template>
+
+<script>
+import CreateOrEdit from './Component/CreateOrEdit'
+export default {
+  name: 'menu-edit',
+  components: {
+    CreateOrEdit
+  }
+}
+</script>
+
+<style>
+
+</style>
