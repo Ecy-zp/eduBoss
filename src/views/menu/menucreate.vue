a6695aac042c1cb09efeb20a5d7f243dd7f2c2e4
Ecy-zp <2763936916@qq.com>
2021-03-20 20:56:30 +0800

    创建菜单页面

---
+++ src/views/menu/menucreate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="menu-create">
+    <create-or-edit :isEdit="false"></create-or-edit>
+  </div>
+</template>
+
+<script>
+import CreateOrEdit from './Component/CreateOrEdit'
+export default {
+  name: 'menu-crate',
+  components: {
+    CreateOrEdit
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
