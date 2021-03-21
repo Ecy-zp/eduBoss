3e10a290e094352898a04ac76ed531b3f6fbc550
Ecy-zp <2763936916@qq.com>
2021-03-21 21:58:34 +0800

    资源页面

--- src/views/resource/index.vue
+++ src/views/resource/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="s">
-    resouce
+  <div class="resource">
+    <Resourcelist></Resourcelist>
   </div>
 </template>
 <script>
+import Resourcelist from './components/list'
 export default {
+  name: 'resource',
+  components: {
+    Resourcelist
+  }
 }
 </script>
 <style lang="scss" scoped>
