0cbb570c39962277bbc1a2c435f6d9a6681de5f3
Ecy-zp <2763936916@qq.com>
2021-03-25 15:54:32 +0800

    用户主页面

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>user</div>
+  <div class="user">
+    <userList></userList>
+  </div>
 </template>
 
 <script>
+import userList from './components/List'
 export default {
-
+  name: 'user',
+  components: {
+    userList
+  }
 }
 </script>
 
