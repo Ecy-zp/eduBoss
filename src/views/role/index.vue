a7393b4f56b8f063990b909b9b6bc6d2ffc113c5
Ecy-zp <2763936916@qq.com>
2021-03-25 15:52:54 +0800

    角色主页面

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="s">
-    role
+    <roleList></roleList>
   </div>
 </template>
 <script>
+import roleList from './components/List'
 export default {
+  name: 'Role',
+  components: {
+    roleList
+  }
 }
 </script>
 <style lang="scss" scoped>
