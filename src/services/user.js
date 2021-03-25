6e419f70b7b6aebbaa6591fdf11001aa1f173f5e
Ecy-zp <2763936916@qq.com>
2021-03-25 15:48:21 +0800

    用户接口更新

--- src/services/user.js
+++ src/services/user.js
@@ -19,3 +19,34 @@ export const getUserInfo = () => {
     url: '/front/user/getInfo'
   })
 }
+
+// 分页查询用户信息
+export const getUserPage = data => {
+  return request({
+    method: 'POST',
+    url: '/boss/user/getUserPages',
+    data
+  })
+}
+
+// 封禁用户
+export const forbidUser = userId => {
+  return request({
+    method: 'POST',
+    url: '/boss/user/forbidUser',
+    params: {
+      userId
+    }
+  })
+}
+
+// 启用用户
+export const enableUser = userId => {
+  return request({
+    method: 'GET',
+    url: '/boss/user/enableUser',
+    params: {
+      userId
+    }
+  })
+}
