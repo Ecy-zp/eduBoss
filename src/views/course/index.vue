66ba92ad66fb5824e15cf4aab276a0719108d90b
Ecy-zp <2763936916@qq.com>
2021-03-28 18:39:52 +0800

    课程主页面

--- src/views/course/index.vue
+++ src/views/course/index.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="s">
-    course
+    <courseList></courseList>
   </div>
 </template>
 <script>
+import courseList from './components/List'
 export default {
+  name: 'course',
+  components: {
+    courseList
+  }
 }
 </script>
 <style lang="scss" scoped>
