1cab93666c4d5cdb74eb15ded298e5ac8d260934
Ecy-zp <2763936916@qq.com>
2021-03-28 18:42:43 +0800

    广告位编辑主页面

---
+++ src/views/advert-space/Edit.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="edit-advertspace">
+    <CreateOrUpdate :id="id"></CreateOrUpdate>
+  </div>
+</template>
+
+<script>
+import CreateOrUpdate from './components/createOrUpdate'
+export default {
+  name: 'editAdvertSpace',
+  components: {
+    CreateOrUpdate
+  },
+  props: {
+    id: {
+      type: [Number, String],
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
