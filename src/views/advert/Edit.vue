30ee0f7a92e27625d2a49e45401b60eaebb40339
Ecy-zp <2763936916@qq.com>
2021-03-28 18:45:31 +0800

    广告编辑页面

---
+++ src/views/advert/Edit.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="create">
+    <craeteOrUpdate :advert_Id="advertId"></craeteOrUpdate>
+  </div>
+</template>
+
+<script>
+import craeteOrUpdate from './components/CreateOrUpdate'
+export default {
+  name: 'edit',
+  components: {
+    craeteOrUpdate
+  },
+  props: {
+    advertId: {
+      type: [Number, String],
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
