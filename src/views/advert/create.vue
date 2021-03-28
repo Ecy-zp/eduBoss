208a099d522bd9a5d4adce61c595821a66ed64d2
Ecy-zp <2763936916@qq.com>
2021-03-28 18:45:59 +0800

    广告创建页面

---
+++ src/views/advert/create.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="advert-create">
+    <craeteOrUpdate></craeteOrUpdate>
+  </div>
+</template>
+
+<script>
+import craeteOrUpdate from './components/CreateOrUpdate'
+export default {
+  name: 'advertCreate',
+  components: {
+    craeteOrUpdate
+  }
+}
+</script>
+
+<style>
+
+</style>
