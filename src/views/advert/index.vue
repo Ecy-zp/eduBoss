fbf4b99c83082c92600976b62b8a8975dd8bc7b5
Ecy-zp <2763936916@qq.com>
2021-03-28 18:45:11 +0800

    广告主页面

--- src/views/advert/index.vue
+++ src/views/advert/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="s">
-    advert
+  <div class="advert">
+    <advertList></advertList>
   </div>
 </template>
 <script>
+import advertList from './components/List'
 export default {
+  name: 'advert',
+  components: {
+    advertList
+  }
 }
 </script>
 <style lang="scss" scoped>
