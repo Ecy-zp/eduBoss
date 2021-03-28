42ee121c827629e7a9aa79c7b7676e6e7936a70e
Ecy-zp <2763936916@qq.com>
2021-03-28 18:43:14 +0800

    广告位创建主页面

---
+++ src/views/advert-space/create.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="create-adSpace">
+    <CreateOrUpdate></CreateOrUpdate>
+  </div>
+</template>
+
+<script>
+import CreateOrUpdate from './components/createOrUpdate'
+export default {
+  name: 'creatAdSpcac',
+  components: {
+    CreateOrUpdate
+  }
+}
+</script>
+
+<style>
+
+</style>
