9743035c945364e0059da115875cb250b374099c
Ecy-zp <2763936916@qq.com>
2021-03-28 18:40:19 +0800

    编辑课程页面

---
+++ src/views/course/Edit.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="edit-course">
+    <createOrEdit
+    isEdit
+    :course-id="courseId"></createOrEdit>
+  </div>
+</template>
+
+<script>
+import createOrEdit from './components/CreateOrEdit'
+export default {
+  name: 'course-edit',
+  components: {
+    createOrEdit
+  },
+  props: {
+    courseId: {
+      type: [Number, String],
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
