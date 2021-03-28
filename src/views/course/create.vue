a7b997fcf5547f93686001ceeeaadaa0bf3354e8
Ecy-zp <2763936916@qq.com>
2021-03-28 18:40:49 +0800

    创建课程页面

---
+++ src/views/course/create.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="create-course">
+    <createOrEdit></createOrEdit>
+  </div>
+</template>
+
+<script>
+import createOrEdit from './components/CreateOrEdit'
+export default {
+  name: 'create',
+  components: {
+    createOrEdit
+  }
+}
+</script>
+
+<style>
+
+</style>
